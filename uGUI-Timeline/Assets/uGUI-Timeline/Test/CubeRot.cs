9f824b782bc278b8a4893b6010984c8049c40ef3
Saina <sainakey5@gmail.com>
2022-10-12 03:42:19 +0900

    Cubeが回転する適当なScript

---
+++ uGUI-Timeline/Assets/uGUI-Timeline/Test/CubeRot.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UGUITimeline
+{
+    public class CubeRot : MonoBehaviour
+    {
+        [SerializeField]
+        [Tooltip("x²‚Ì‰ñ“]Šp“x")]
+        private float rotateX = 0;
+    
+        [SerializeField]
+        [Tooltip("y²‚Ì‰ñ“]Šp“x")]
+        private float rotateY = 0;
+
+        [SerializeField]
+        [Tooltip("z²‚Ì‰ñ“]Šp“x")]
+        private float rotateZ = 0;
+
+        // Update is called once per frame
+        void Update()
+        {
+            // X,Y,Z²‚É‘Î‚µ‚Ä‚»‚ê‚¼‚êAw’è‚µ‚½Šp“x‚¸‚Â‰ñ“]‚³‚¹‚Ä‚¢‚éB
+            // deltaTime‚ğ‚©‚¯‚é‚±‚Æ‚ÅAƒtƒŒ[ƒ€‚²‚Æ‚Å‚Í‚È‚­A1•b‚²‚Æ‚É‰ñ“]‚·‚é‚æ‚¤‚É‚µ‚Ä‚¢‚éB
+            gameObject.transform.Rotate(new Vector3(rotateX, rotateY, rotateZ) * Time.deltaTime);
+        }
+    }
+}
