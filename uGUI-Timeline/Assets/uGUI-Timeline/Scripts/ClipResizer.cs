38aad4ca9856df1495707e2eae3fa7762d1b6b10
Saina <sainakey5@gmail.com>
2022-10-13 06:15:37 +0900

    クリップのリサイズ実装

--- uGUI-Timeline/Assets/uGUI-Timeline/Scripts/ClipResizer.cs
+++ uGUI-Timeline/Assets/uGUI-Timeline/Scripts/ClipResizer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,43 +9,63 @@ namespace UGUITimeline
     public class ClipResizer : MonoBehaviour , IDragHandler
     {
         [SerializeField] private bool isRight;
-        [Space]
+        [Space] 
+        [SerializeField] private Camera camera;
         [SerializeField] private Canvas canvas;
         [SerializeField] private RectTransform clipRectTrans;
+
+        private void Start()
+        {
+            //ref:https://light11.hatenadiary.com/entry/2019/04/16/003642
+            canvas = GetComponentInParent<Canvas>();
+            
+            if (canvas.renderMode == RenderMode.ScreenSpaceOverlay) {
+                camera = null;
+            }
+            else
+            {
+                camera = canvas.worldCamera;
+            }
+        }
+
         public void OnDrag(PointerEventData eventData)
         {
-            /*
-            clipRectTrans.anchorMin = Vector2.zero;
-            clipRectTrans.anchorMax = Vector2.one;
-            */
-
-            //var pos = edge.localPosition;
-            var deltaPos = new Vector3(eventData.delta.x, eventData.delta.y, 0) / canvas.scaleFactor;
-            //edge.localPosition = pos + deltaPos;
+            //clipの端のworldPosとマウスのworldPosを得てなんやかんやすると良さそう
+            //var deltaPos = new Vector3(delta.x, delta.y, 0) / (canvas.scaleFactor);
+            var screenPoint = eventData.position;
+            var worldPoint = Vector3.zero;
+            RectTransformUtility.ScreenPointToWorldPointInRectangle(clipRectTrans, screenPoint, camera, out worldPoint);
+            
+            //rectの右上取りたい
+            Vector3[] v = new Vector3[4];
+            clipRectTrans.GetWorldCorners(v);
+            //Debug.Log("右上"+v[3]);
+            
             if (isRight)
             {
+                var deltaPos = v[3].x - worldPoint.x / canvas.scaleFactor;
                 var sizeDelta = clipRectTrans.sizeDelta;
-                sizeDelta.x += (deltaPos.x * 2.0f / 3.0f);
+                sizeDelta.x -= (deltaPos * 2.0f / 3.0f) ;
                 clipRectTrans.sizeDelta = sizeDelta;
 
                 var localPos = clipRectTrans.localPosition;
-                localPos.x += (deltaPos.x * 1.0f / 3.0f);
+                localPos.x -= (deltaPos * 1.0f / 3.0f) ;
                 clipRectTrans.localPosition = localPos;
             }
             else
             {
+                var deltaPos = v[0].x - worldPoint.x / canvas.scaleFactor;
                 var sizeDelta = clipRectTrans.sizeDelta;
-                sizeDelta.x -= (deltaPos.x * 2.0f / 3.0f);
+                sizeDelta.x += (deltaPos * 2.0f / 3.0f) ;
                 clipRectTrans.sizeDelta = sizeDelta;
 
                 var localPos = clipRectTrans.localPosition;
-                localPos.x += (deltaPos.x * 1.0f / 3.0f);
+                localPos.x -= (deltaPos * 1.0f / 3.0f) ;
                 clipRectTrans.localPosition = localPos;
             }
             
-            
+
         }
-        
         
     }
 }
